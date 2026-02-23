b6762fb973c9c36981da9f2383ab752c82fded2c
Your Name <you@example.com>
2026-02-23 20:21:27 +0100

    Ajout du calcul du pourcentage de transactions par type et de la détection des transactions anormales dans les requêtes SQL

--- sql/02_analysis_queries.sql
+++ sql/02_analysis_queries.sql
@@ -9,11 +9,39 @@ ORDER BY month;
 --Nombre de transactions par type--
 SELECT
 	transaction_type ,
-	count(*) AS nb_transactions
+	count(*) AS nb_transactions,
+	ROUND(
+        100.0 * COUNT(*) / SUM(COUNT(*)) OVER (),
+        2
+    ) as pourcentage_total
 FROM
 	transactions 
 GROUP BY	
     transaction_type
 ORDER BY
     nb_transactions DESC;
+
+--Solde moyen par type de compte--
+SELECT
+	account_type ,
+	ROUND(AVG(account_balance), 2) AS solde_moyen
+FROM
+	accounts
+GROUP BY	
+    account_type
+ORDER BY
+    solde_moyen DESC;
+
+--Détection de transaction anormale--
+--j'ai mis 3500 car il y'a pas de transactions qui dépasse 3500 dans la bdd--
+SELECT
+	transaction_id ,
+	account_id,
+	transaction_date,
+	amount AS transaction_anormale,
+	transaction_type
+FROM
+	transactions 
+WHERE
+	amount > 3500 ;
 
